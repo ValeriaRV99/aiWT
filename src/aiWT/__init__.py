2f31137772f0f32f31aa5f65c89d8076adde9ac1
Valeria Rios Vargas <vr371@scarletmail.rutgers.edu>
2023-09-11 14:05:10 -0600

    update functional in init file

--- src/aiWT/__init__.py
+++ src/aiWT/__init__.py
@@ -1,12 +1,10 @@
 __author__ = "Pavanello Research Group"
-__contact__ = "m.pavanello@rutgers.edu"
+__contact__ = ""
 __license__ = "MIT"
 __version__ = "0.0.1"
 __date__ = "2023-09-11"
 
-from .config import *
-from .mpi import mp, sprint
-from .time_data import TimeData, timer
+from .aiWT import *
 
 try:
     from importlib.metadata import version # python >= 3.8
@@ -17,6 +15,6 @@ except Exception :
         pass
 
 try:
-    __version__ = version("siWT")
+    __version__ = version("aiWT")
 except Exception :
     pass
